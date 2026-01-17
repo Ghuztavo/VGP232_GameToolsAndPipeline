e2fd9e57228715fc21daba03089cfea17bcdbc25
Ghuztavo <ghuztavo2002@gmail.com>
2026-01-16 18:41:32 -0800

    Add project files.

---
+++ Assignment2a/Weapon.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2a
+{
+    public class Weapon
+    {
+        public enum WeaponType
+        {
+            Sword, Polearm, Claymore, Catalyst, Bow, None
+        }
+
+        // Name,Type,Rarity,BaseAttack
+        public string Name { get; set; }
+        public WeaponType Type { get; set; }
+        public int Rarity { get; set; }
+        public int BaseAttack { get; set; }
+        public string Image { get; set; }
+        public string SecondaryStat { get; set; }
+        public string Passive { get; set; }
+
+        /// <summary>
+        /// The Comparator function to check for name
+        /// </summary>
+        /// <param name="left">Left side Weapon</param>
+        /// <param name="right">Right side Weapon</param>
+        /// <returns> -1 (or any other negative value) for "less than", 0 for "equals", or 1 (or any other positive value) for "greater than"</returns>
+        public static int CompareByName(Weapon left, Weapon right)
+        {
+            return left.Name.CompareTo(right.Name);
+        }
+
+        public static int CompareByType(Weapon left, Weapon right)
+        {
+            return left.Type.CompareTo(right.Type);
+        }
+
+        public static int CompareByRarity(Weapon left, Weapon right)
+        {
+            return left.Rarity.CompareTo(right.Rarity);
+        }
+
+        public static int CompareByBaseAttack(Weapon left, Weapon right)
+        {
+            return left.BaseAttack.CompareTo(right.BaseAttack);
+        }
+
+        /// <summary>
+        /// The Weapon string with all the properties
+        /// </summary>
+        /// <returns>The Weapon formated string</returns>
+        public override string ToString()
+        {
+            // TODO: construct a comma seperated value string
+            // Name,Type,Image,Rarity,BaseAttack,SecondaryStat,Passive
+
+            return $"{Name},{Type},{Image},{Rarity},{BaseAttack},{SecondaryStat},{Passive}";
+        }
+
+        public static bool TryParse(string rawData, out Weapon weapon)
+        {
+            // TODO: validate that the string array the size expected.
+            // TODO: use int.Parse or TryParse for stats/number values.
+            // Populate the properties of the Weapon
+            // TODO: Add the Weapon to the list
+
+            weapon = null;
+
+            if (string.IsNullOrWhiteSpace(rawData))
+            {
+                Console.WriteLine("Input data is null or empty.");
+                return false;
+            }
+
+            // Order:
+            // Name,Type,Image,Rarity,BaseAttack,SecondaryStat,Passive
+            string[] values = rawData.Split(',');
+
+            if (values.Length != 7)
+            {
+                Console.WriteLine($"Input data does not have the correct number of fields: {rawData}");
+                return false;
+            }
+
+            // Parse WeaponType
+            if (!Enum.TryParse(values[1], true, out WeaponType type))
+            {
+                Console.WriteLine($"Invalid WeaponType value: {values[1]}");
+                type = WeaponType.None;
+            }
+
+            // Parse Rarity
+            if (!int.TryParse(values[3], out int rarity))
+            {
+                Console.WriteLine($"Invalid Rarity value: {values[3]}");
+                return false;
+            }
+
+            // Parse BaseAttack
+            if (!int.TryParse(values[4], out int baseAttack))
+            {
+                Console.WriteLine($"Invalid BaseAttack value: {values[4]}");
+                return false;
+            }
+
+            weapon = new Weapon
+            {
+                Name = values[0],
+                Type = type,
+                Image = values[2],
+                Rarity = rarity,
+                BaseAttack = baseAttack,
+                SecondaryStat = values[5],
+                Passive = values[6]
+            };
+
+            return true;
+        }
+    }
+}
---
+++ Assignment2a/WeaponCollection.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WeaponType = Assignment2a.Weapon.WeaponType;
+
+namespace Assignment2a
+{
+    class WeaponCollection : List<Weapon>, IPeristence
+    {
+        public bool Load(string filename)
+        {
+            if (!File.Exists(filename))
+                return false;
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(filename))
+                {
+                    reader.ReadLine(); // Skip header
+
+                    // Read each line
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+
+                        if (Weapon.TryParse(line, out Weapon weapon))
+                        {
+                            this.Add(weapon);
+                        }
+                        // invalid rows are ignored but handled inside TryParse
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading file: {ex.Message}");
+                return false;
+            }
+        }
+
+        public bool Save(string filename)
+        {
+            return true;
+        }
+
+        // get the highest base attack
+        public int GetHighestBaseAttack()
+        {
+            int highest = 0;
+            foreach (var weapon in this)
+            {
+                if (weapon.BaseAttack > highest)
+                {
+                    highest = weapon.BaseAttack;
+                }
+            }
+            return highest;
+        }
+
+
+        // get lowest base attack
+        public int GetLowestBaseAttack()
+        {
+            int lowest = int.MaxValue;
+            foreach (var weapon in this)
+            {
+                if (weapon.BaseAttack < lowest)
+                {
+                    lowest = weapon.BaseAttack;
+                }
+            }
+            return lowest;
+        }
+
+        // get all weapons of a specific type
+        public List<Weapon> GetAllWeaponsOfType(WeaponType type)
+        {
+            List<Weapon> weaponsOfType = new List<Weapon>();
+            foreach (var weapon in this)
+            {
+                if (weapon.Type == type)
+                {
+                    weaponsOfType.Add(weapon);
+                }
+            }
+            return weaponsOfType;
+        }
+
+        public void SortBy(string columnName)
+        {
+            Comparison<Weapon> comparison = columnName.ToLower() switch
+            {
+                "name" => Weapon.CompareByName,
+                "type" => Weapon.CompareByType,
+                "rarity" => Weapon.CompareByRarity,
+                "baseattack" => Weapon.CompareByBaseAttack,
+                _ => throw new ArgumentException($"Invalid column name: {columnName}")
+            };
+            this.Sort(comparison);
+        }
+
+    }
+}
+
